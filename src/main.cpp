49e9ef964ceb7ce13d135926f721f0b75c00daae
seva <vsevolod.synytskyi@gmail.com>
2026-07-24 11:29:48 +0300

    boot button introduced

--- src/main.cpp
+++ src/main.cpp
@@ -7,17 +7,23 @@ const int RED_LED_PIN = 16;
 // External button pin
 const int BUTTON_PIN = 17;
 
+// BOOT button is hard-wired to GPIO0 on the board (pull-up + button to GND).
+// Reads HIGH when released, LOW when pressed.
+const int BOOT_BUTTON_PIN = 0;
+
 void setup() {
   Serial.begin(115200);
 
   pinMode(RED_LED_PIN, OUTPUT);
   pinMode(BLUE_LED_PIN, OUTPUT);
   pinMode(BUTTON_PIN, INPUT);
+  pinMode(BOOT_BUTTON_PIN, INPUT_PULLUP);
 
   Serial.println("Board started!");
 }
 
 int lastButtonState = -1;
+int lastBootState = -1;
 
 void loop() {
   int buttonState = digitalRead(BUTTON_PIN);
@@ -42,5 +48,16 @@ void loop() {
       Serial.println("Button not pressed -> RED on");
     }
     lastButtonState = buttonState;
+  }
+
+  // BOOT button (GPIO0): LOW = pressed, HIGH = released
+  int bootState = digitalRead(BOOT_BUTTON_PIN);
+  if (bootState != lastBootState) {
+    if (bootState == LOW) {
+      Serial.println("BOOT button pressed");
+    } else {
+      Serial.println("BOOT button released");
+    }
+    lastBootState = bootState;
   }
 }
